e4c6734244ec25f85398916c367e89951756fa25
quagly <quagly@gmail.com>
2017-08-26 12:39:58 +0000

    added dependencies for cookbook examples

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [iota "1.1.3"]]
   :main ^:skip-aot learn-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
