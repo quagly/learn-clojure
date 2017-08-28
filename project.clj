dfef11b60811e1a3e19925de3e2035d898913a0e
quagly <quagly@gmail.com>
2017-08-28 12:03:30 +0000

    added comments to project

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 ; for example 88 logging 
                  [com.taoensso/timbre "4.10.0"]
+                 ; for example 413 parallel reducer file processing
                  [iota "1.1.3"]]
   :main ^:skip-aot learn-clojure.core
   :target-path "target/%s"
